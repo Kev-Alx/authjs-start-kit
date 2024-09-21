fc675fd8228b0a0e6a47e30967415069ef020a58
Kev-Alx <120734048+Kev-Alx@users.noreply.github.com>
2024-09-21 14:12:25 +0700

    archive

--- lib/mail.ts
+++ lib/mail.ts
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-password?token=${token}`;
-
+  
   await resend.emails.send({
     from: "onboarding@resend.dev",
     to: email,
